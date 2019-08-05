da69e50d35209ecbbf89fe66899d01446abb143f
frannuca <frannuca@gmail.com>
2019-08-05 23:43:02 +0200

    remove build

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/venus/code/cpp/cnn/tests
-# Build directory: /mnt/c/Users/venus/code/cpp/cnn/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_queue "test_queue")
-set_tests_properties(test_queue PROPERTIES  FAIL_REGULAR_EXPRESSION "(Exception|Test failed)" PASS_REGULAR_EXPRESSION "OK" TIMEOUT "120")
